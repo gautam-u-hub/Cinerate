cfa63af765bf658c34d1771e99c4f3d20d5de495
gautam-u-hub <72206790+gautam-u-hub@users.noreply.github.com>
2023-03-17 19:28:53 +0530

    Update auth.js

--- Backend/middleware/auth.js
+++ Backend/middleware/auth.js
@@ -10,7 +10,7 @@ exports.isAuthenticatedUser = catchAsyncErrors(async (req, res, next) => {
     return next(new ErrorHander("Please Login to access this resource", 401));
   }
 
-  const decodedData = jwt.verify(token, process.env.JWT_SECRET);
+  const decodedData = jwt.verify(token, "euhjhnffv");
 
   req.user = await User.findById(decodedData.id);
 
